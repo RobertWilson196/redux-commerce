27818bddc9086f37bb44d31a14c267c21aec06a6
Robert Wilson <youmufan@gmail.com>
2018-03-08 20:03:49 -0500

    brought product object  from ProductList to Product

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,19 +1,9 @@
 import React from 'react';
-
-
-const Product = props => {
-    return(
-        <li>
-            <img src={props.imgSrc} alt={props.name} />
-            <p>{props.name}</p>
-            <p>${props.price}</p>
-            <button onClick = {() => console.log("+")}>+</button>
-            <button onClick = {() => console.log("-")}>-</button>
-        </li>
-    );
-}
+import Product from './Product';
+z
 const ProductList = props => {
-    const ProductItems = props.products.map((product, index) => <Product />);
+    const ProductItems = props.products.map((product, index) =>
+     <Product {...product} key={index} />);
     return (
         <ul>
             <li>{ProductItems}</li>
