daf204b1960ede6f44da9cd865f0ccf0d5c18483
Robert Wilson <youmufan@gmail.com>
2018-03-08 20:33:03 -0500

    connected the product component and provided the ability to dispatch ADD_ITEM and REMOVE_ITEM actions

--- src/Product.js
+++ src/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 const Product = props => {
     return(
@@ -6,11 +7,16 @@ const Product = props => {
             <img src={props.imgSrc} alt={props.name} />
             <p>{props.name}</p>
             <p>${props.price}</p>
-            <button onClick = {() => console.log("+")}>+</button>
-            <p>{props.count}</p>
-            <button onClick = {() => console.log("-")}>-</button>
+            <button onClick = {() => props.dispatch({type: 'ADD_ITEM', 
+                                     payload: { value: props.position }})}>+</button>
+            <span>{props.count}</span>
+            <button onClick = {() => props.dispatch({type: 'REMOVE_ITEM',
+                                     payload: { value: props.position }})}>-</button>
         </li>
     );
 }
 
-export default Product;
+const mapStateToProps = null;
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Product from './Product';
-z
+
 const ProductList = props => {
     const ProductItems = props.products.map((product, index) =>
-     <Product {...product} key={index} />);
+     <Product {...product} key={index} position={index}/>);
     return (
         <ul>
             <li>{ProductItems}</li>
