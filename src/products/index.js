eff6e74e4675adee61687c753f89b1181438a6e6
Robert Wilson <youmufan@gmail.com>
2018-03-08 19:27:33 -0500

    created mock initial state for products

--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,29 @@
 
 
-const reducer = (state={}, action) => {
+const initialState = [
+
+    {
+        imgSrc: 'http://via.placeholder.com/100x100',
+        name: "Javelin",
+        price: 500.00,
+        count: 0
+    },
+    {
+        imgSrc: 'http://via.placeholder.com/100x100',
+        name: "Snail",
+        price: 3.99,
+        count: 0
+    },
+    {
+        imgSrc: 'http://via.placeholder.com/100x100',
+        name: "Rug",
+        price: 93.47,
+        count: 0
+    }
+
+];
+
+const reducer = (state=initialState, action) => {
     console.log('products');
     return state;
 }
